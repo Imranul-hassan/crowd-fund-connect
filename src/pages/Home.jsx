9dc019acf619c2700f0428cbc6d872ac9730aa39
Akib <imranulhassan3296@gmail.com>
2024-12-08 13:02:59 +0600

    added home page typewriter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,29 +2,56 @@ import { useLoaderData } from "react-router-dom";
 import CampaignCard from "../components/CampaignCard";
 import Slider from "../components/Slider";
 import FAQ from "../components/FAQ";
+import { Typewriter } from 'react-simple-typewriter'
 
 
 const Home = () => {
     const campaigns = useLoaderData();
-    return (
-        <div>
-           <Slider></Slider>
-           <div className="mt-10 mb-8">
-                <h3 className="text-4xl font-bold text-center">Still Time to Give: Ongoing Campaigns</h3>
-                <p className="text-center mx-auto mt-3 text-xl md:w-5/12 ">Explore ongoing campaigns that need your support. Every contribution makes a difference—join the movement today!</p>
-            </div>
-            <div className="grid md:grid-cols-3 gap-4">
-                {
-                    
-                    campaigns.map(campaign => <CampaignCard key={campaign._id} campaign={campaign}>
+    const handleType = (count) => {
+        // Logs each typewriter step count
+        console.log(`Current step: ${count}`);
+    };
 
-                    </CampaignCard>)
-                  
-                }  
-            </div>
-            <FAQ></FAQ>
+const handleDone = () => {
+    console.log(`Done after 5 loops!`)
+}
+
+return (
+    <div>
+        <Slider></Slider>
+
+        <div className=" mb-8">
+            <h1 className="text-4xl text-center " style={{ paddingTop: '2rem', margin: 'auto 0', fontWeight: 'bold' }}>
+             Ongoing Campaigns{' '}
+                <span style={{ color: 'red', fontWeight: 'bold' }}>
+                    {/* Style will be inherited from the parent element */}
+                    <Typewriter
+                        words={['Personal Needs', 'Creative Ideas', 'Startups']}
+                        loop={10}
+                        cursor
+                        cursorStyle='_'
+                        typeSpeed={70}
+                        deleteSpeed={50}
+                        delaySpeed={1000}
+                        onLoopDone={handleDone}
+                        onType={(count) => handleType(count)}
+                    />
+                </span>
+            </h1>
+            <p className="text-center mx-auto mt-3 text-xl md:w-5/12 ">Explore ongoing campaigns that need your support. Every contribution makes a difference—join the movement today!</p>
         </div>
-    );
+        <div className="grid md:grid-cols-3 gap-4">
+            {
+
+                campaigns.map(campaign => <CampaignCard key={campaign._id} campaign={campaign}>
+
+                </CampaignCard>)
+
+            }
+        </div>
+        <FAQ></FAQ>
+    </div>
+);
 };
 
 export default Home;
